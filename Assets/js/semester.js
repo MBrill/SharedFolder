ed5a336933e357250e9b4d5698796d951c3c1915
MBrill <manfred.brill@hs-kl.de>
2022-02-07 14:08:46 +0100

    Semester umgestellt
    E-Books zu R redigiert, und in RMarkdown die erste Shiny-App
    deployed.
    
    On branch develop
    Your branch is up to date with 'origin/develop'.
    
    Changes to be committed:
    modified:   Assets/js/semester.js
    modified:   Assets/js/theSemester.json
    modified:   R/ebooks.html
    modified:   RMarkdown

--- Assets/js/semester.js
+++ Assets/js/semester.js
@@ -1,4 +1,4 @@
-var theSemester = { semester : "Sommersemester 2021"};
+var theSemester = { semester : "Sommersemester 2022"};
 
 // Ausgabe des aktuellen Semesters in eine geöffnete HTML-Datei
 function getSemester() 
